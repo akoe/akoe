11044612904ecbb96fad60f0c4328f0a4b804dc3
Matt Sonic <matt@sonictransfer.com>
2011-05-19 15:32:39 -0700

    initial commit

---
+++ lib/job/queue.rb
@@ -0,0 +1,34 @@
+require 'aws'
+
+module FFMpeg
+  class Queue
+    attr_reader :title
+
+    def initialize(title)
+      access_id = ENV["S3_ACCESS_ID"]
+      secret_key = ENV["S3_SECRET_KEY"]
+
+      @sqs = Aws::Sqs.new(access_id, secret_key)
+      @title = title
+      @queue = @sqs.queue(title, true)
+    end
+
+    def size
+      @queue.size
+    end
+
+    def clear
+      # NOTE: Any message within the visibility timeout will not be deleted!
+      @queue.clear
+    end
+
+    def send_message(msg)
+      @queue.send_message(msg)
+    end
+
+    def receive_message
+      message = @queue.receive
+      message.body unless message.nil?
+    end
+  end
+end
---
+++ spec/job/queue_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe FFMpeg::Queue do
+  subject { FFMpeg::Queue.new("my_queue") }
+  before(:all) do
+    subject.clear # you must delete and rebuild a queue to *absolutely* clear it
+  end
+  it "should read queue size" do
+    subject.should respond_to(:size)
+  end
+  it "should clear all messages" do
+    subject.clear
+  end
+  it "should send a message" do
+    subject.send_message("hey hey")
+  end
+  it "should read a message" do
+    subject.send_message("hey hey")
+    subject.receive_message.should == "hey hey"
+  end
+end
